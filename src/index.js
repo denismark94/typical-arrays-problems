d90917b2474b5ce2f889ed74b08a9c03d5ca6903
denismark94 <denismark94@gmail.com>
2021-03-14 18:46:36 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+// function min (array) {
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  var min = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] < min) {min = array[i];}
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  var max = array[0];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > max) {max = array[i];}
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  var avg = 0;
+  for (i = 0; i < array.length; i++) { avg += array[i];};
+  return avg / array.length;
 }
+
+// console.log(min([]))
